1467699976d14c93870f16956f22475ff7974e15
Vincent Schmitt <vschmitt@fortinet.com>
2025-09-12 14:39:56 +0200

    other small fixes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -24,6 +24,7 @@ import {
 } from './queries';
 import { scrapeProductData } from './scraper';
 import authMiddleware from './authMiddleware';
+import bcrypt from 'bcryptjs/umd/types';
 
 
 const router = express.Router();
